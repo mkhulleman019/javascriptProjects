d60d78dd50f71065446dbc01336dfe63911d8c16
mkhulleman019 <mkhulleman019@yahoo.com>
2021-12-30 11:22:52 -0700

    added index.html and main.js

---
+++ basicJavaScriptProjects/project1ExpressionsAlert/js/main.js
@@ -0,0 +1,15 @@
+window.alert("Spoiler Alert! JavaScript Ahead");
+
+var family="The Hullemans", dad="Bradley", mom="MaKayla", daughter="Olivia", son="Kieran"; //assigning names to variables for each family member//
+var family= family.fontcolor("green");//styling var family with green text//
+var dad= dad.fontcolor("blue");//styling var dad with blue text//
+var mom= mom.fontcolor("violet");//styling var mom with violet text//
+var daughter = daughter.fontcolor("pink");//styling var daughteer with pink text//
+var son = son.fontcolor("orange");//styling var son with orange text//
+document.write(family + " " + dad + " " + mom + " " + daughter + " " + son)//printing all variables in their styled colors//
+
+
+document.write("<br><br>"); //adding line breaks//
+
+document.write("1 \+ 3 \+ 5 = ") //printing the equation, answer executed in statement below//
+document.write(1+3+5);//executing a statement with an expression//
